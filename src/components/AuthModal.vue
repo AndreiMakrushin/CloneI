f365c3c90c410aa8ffe2b742d3118bdf8cf6f824
Andrei <andrei18makrushin@gmail.com>
2023-04-08 20:15:38 +0500

    function handleSignup

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,38 +1,47 @@
 <script setup>
-  import { ref } from 'vue';
+import { ref } from "vue";
+import { useUserStore } from "../stores/users";
 
-    const props = defineProps(['isLogin'])
+const userStore = useUserStore();
+const {errorMessage, handleSignup} = userStore
+const props = defineProps(["isLogin"]);
 
-      const visible = ref(false);
-      const showModal = () => {
-        visible.value = true;
-      };
-      const handleOk = () => {
-        visible.value = false;
-      };
-      const title = props.isLogin ? 'Войти' : 'Зарегистрироваться'
+const visible = ref(false);
+const showModal = () => {
+  visible.value = true;
+};
+const handleOk = () => {
+  visible.value = false;
+};
+const title = props.isLogin ? "Войти" : "Зарегистрироваться";
 
-      const name = ref("")
-      const pass = ref("")
-      const email = ref("")
-  </script>
+const name = ref("");
+const pass = ref("");
+const email = ref("");
+</script>
 <template>
-    <div>
-      <a-button type="primary" @click="showModal" class="btn">{{title}}</a-button>
-      <a-modal v-model:visible="visible" :title="title" @ok="handleOk">
-        <a-input class="input" v-if="!isLogin" v-model:value="name" placeholder="Username" />
-        <a-input class="input" v-model:value="pass" placeholder="Pass" />
-        <a-input class="input" v-model:value="email" placeholder="Email" />
-      </a-modal>
-    </div>
-  </template>
+  <div>
+    <a-button type="primary" @click="showModal" class="btn">{{
+      title
+    }}</a-button>
+    <a-modal v-model:visible="visible" :title="title" @ok="handleOk">
+      <a-input
+        class="input"
+        v-if="!isLogin"
+        v-model:value="name"
+        placeholder="Username"
+      />
+      <a-input class="input" v-model:value="pass" placeholder="Pass" />
+      <a-input class="input" v-model:value="email" placeholder="Email" />
+    </a-modal>
+  </div>
+</template>
 
-  <style scoped>
-  .btn{
-    margin-left: 10px;
-  }
-  .input{
-    margin-top: 10px;
-  }
-  </style>
-  
+<style scoped>
+.btn {
+  margin-left: 10px;
+}
+.input {
+  margin-top: 10px;
+}
+</style>
--- src/stores/users.js
+++ src/stores/users.js
@@ -1,13 +1,37 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
+export const useUserStore = defineStore('users', () => {
   const user = ref(null)
+  const errorMessage = ref("")
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
   const handleLogin = () => {}
-  const handleSignup = () => {}
+
+  const handleSignup = (credentials) => {
+    const {email, password, username} = credentials
+
+    if (password.length < 6) {
+      return errorMessage.value = "Пароль слишком короткий"
+    }
+    if (username.length < 4) {
+      return errorMessage.value = "Имя пользователя, слишком короткое"
+    }
+    if(!validateEmail(email)){
+      return errorMessage.value = "проверьте правильность заполнения электронной почты"
+    }
+  }
+
   const handleLogout = () => {}
+
   const getUser = () => {}
   
-  return { user, handleLogin, handleSignup, handleLogout, getUser}
+  return { user, errorMessage, handleLogin, handleSignup, handleLogout, getUser}
 })
